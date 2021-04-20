50cec2d333669deb8c2485ad55a592a969d0c4f9
Vikash Kumar Prajapati <vikkurohit10357@gmail.com>
2021-04-20 16:39:42 +0530

    Basic UI completed

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -29,13 +29,266 @@ namespace TDP_Vista_Software_UI
 		/// </summary>
 		private void InitializeComponent()
 		{
-			this.components = new System.ComponentModel.Container();
+			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+			this.panel1 = new System.Windows.Forms.Panel();
+			this.panel2 = new System.Windows.Forms.Panel();
+			this.pictureBox2 = new System.Windows.Forms.PictureBox();
+			this.pictureBox1 = new System.Windows.Forms.PictureBox();
+			this.panel3 = new System.Windows.Forms.Panel();
+			this.button2 = new System.Windows.Forms.Button();
+			this.button1 = new System.Windows.Forms.Button();
+			this.rs_btn1 = new System.Windows.Forms.Button();
+			this.pictureBox3 = new System.Windows.Forms.PictureBox();
+			this.pictureBox4 = new System.Windows.Forms.PictureBox();
+			this.panel4 = new System.Windows.Forms.Panel();
+			this.label1 = new System.Windows.Forms.Label();
+			this.label2 = new System.Windows.Forms.Label();
+			this.label3 = new System.Windows.Forms.Label();
+			this.panel2.SuspendLayout();
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+			this.panel3.SuspendLayout();
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
+			this.panel4.SuspendLayout();
+			this.SuspendLayout();
+			// 
+			// panel1
+			// 
+			this.panel1.BackColor = System.Drawing.SystemColors.GrayText;
+			this.panel1.Dock = System.Windows.Forms.DockStyle.Left;
+			this.panel1.Location = new System.Drawing.Point(0, 0);
+			this.panel1.Name = "panel1";
+			this.panel1.Size = new System.Drawing.Size(47, 572);
+			this.panel1.TabIndex = 0;
+			// 
+			// panel2
+			// 
+			this.panel2.BackColor = System.Drawing.SystemColors.GrayText;
+			this.panel2.Controls.Add(this.pictureBox4);
+			this.panel2.Controls.Add(this.pictureBox3);
+			this.panel2.Controls.Add(this.pictureBox2);
+			this.panel2.Dock = System.Windows.Forms.DockStyle.Top;
+			this.panel2.Location = new System.Drawing.Point(47, 0);
+			this.panel2.Name = "panel2";
+			this.panel2.Size = new System.Drawing.Size(1068, 29);
+			this.panel2.TabIndex = 1;
+			// 
+			// pictureBox2
+			// 
+			this.pictureBox2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+			this.pictureBox2.Cursor = System.Windows.Forms.Cursors.Hand;
+			this.pictureBox2.Dock = System.Windows.Forms.DockStyle.Right;
+			this.pictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox2.Image")));
+			this.pictureBox2.Location = new System.Drawing.Point(1036, 0);
+			this.pictureBox2.Name = "pictureBox2";
+			this.pictureBox2.Size = new System.Drawing.Size(32, 29);
+			this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+			this.pictureBox2.TabIndex = 3;
+			this.pictureBox2.TabStop = false;
+			this.pictureBox2.Click += new System.EventHandler(this.pictureBox2_Click);
+			// 
+			// pictureBox1
+			// 
+			this.pictureBox1.BackColor = System.Drawing.SystemColors.GrayText;
+			this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+			this.pictureBox1.Location = new System.Drawing.Point(0, 0);
+			this.pictureBox1.Name = "pictureBox1";
+			this.pictureBox1.Size = new System.Drawing.Size(139, 29);
+			this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+			this.pictureBox1.TabIndex = 0;
+			this.pictureBox1.TabStop = false;
+			// 
+			// panel3
+			// 
+			this.panel3.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+			this.panel3.Controls.Add(this.button2);
+			this.panel3.Controls.Add(this.button1);
+			this.panel3.Controls.Add(this.rs_btn1);
+			this.panel3.Dock = System.Windows.Forms.DockStyle.Right;
+			this.panel3.Location = new System.Drawing.Point(1057, 29);
+			this.panel3.Name = "panel3";
+			this.panel3.Size = new System.Drawing.Size(58, 543);
+			this.panel3.TabIndex = 2;
+			// 
+			// button2
+			// 
+			this.button2.AllowDrop = true;
+			this.button2.BackColor = System.Drawing.SystemColors.GrayText;
+			this.button2.Dock = System.Windows.Forms.DockStyle.Top;
+			this.button2.FlatAppearance.BorderColor = System.Drawing.Color.Gray;
+			this.button2.FlatAppearance.BorderSize = 0;
+			this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+			this.button2.Font = new System.Drawing.Font("Comic Sans MS", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+			this.button2.ForeColor = System.Drawing.SystemColors.Control;
+			this.button2.Location = new System.Drawing.Point(0, 72);
+			this.button2.Name = "button2";
+			this.button2.Size = new System.Drawing.Size(58, 36);
+			this.button2.TabIndex = 5;
+			this.button2.Text = "Btn 3";
+			this.button2.TextAlign = System.Drawing.ContentAlignment.TopRight;
+			this.button2.UseVisualStyleBackColor = false;
+			// 
+			// button1
+			// 
+			this.button1.AllowDrop = true;
+			this.button1.BackColor = System.Drawing.SystemColors.GrayText;
+			this.button1.Dock = System.Windows.Forms.DockStyle.Top;
+			this.button1.FlatAppearance.BorderColor = System.Drawing.Color.Gray;
+			this.button1.FlatAppearance.BorderSize = 0;
+			this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+			this.button1.Font = new System.Drawing.Font("Comic Sans MS", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+			this.button1.ForeColor = System.Drawing.SystemColors.Control;
+			this.button1.Location = new System.Drawing.Point(0, 36);
+			this.button1.Name = "button1";
+			this.button1.Size = new System.Drawing.Size(58, 36);
+			this.button1.TabIndex = 4;
+			this.button1.Text = "Btn 2";
+			this.button1.TextAlign = System.Drawing.ContentAlignment.TopRight;
+			this.button1.UseVisualStyleBackColor = false;
+			// 
+			// rs_btn1
+			// 
+			this.rs_btn1.AllowDrop = true;
+			this.rs_btn1.BackColor = System.Drawing.SystemColors.GrayText;
+			this.rs_btn1.Dock = System.Windows.Forms.DockStyle.Top;
+			this.rs_btn1.FlatAppearance.BorderColor = System.Drawing.Color.Gray;
+			this.rs_btn1.FlatAppearance.BorderSize = 0;
+			this.rs_btn1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+			this.rs_btn1.Font = new System.Drawing.Font("Comic Sans MS", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+			this.rs_btn1.ForeColor = System.Drawing.SystemColors.Control;
+			this.rs_btn1.Location = new System.Drawing.Point(0, 0);
+			this.rs_btn1.Name = "rs_btn1";
+			this.rs_btn1.Size = new System.Drawing.Size(58, 36);
+			this.rs_btn1.TabIndex = 3;
+			this.rs_btn1.Text = "Btn 1";
+			this.rs_btn1.TextAlign = System.Drawing.ContentAlignment.TopRight;
+			this.rs_btn1.UseVisualStyleBackColor = false;
+			this.rs_btn1.Paint += new System.Windows.Forms.PaintEventHandler(this.rs_btn1_Paint);
+			// 
+			// pictureBox3
+			// 
+			this.pictureBox3.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+			this.pictureBox3.Cursor = System.Windows.Forms.Cursors.Hand;
+			this.pictureBox3.Dock = System.Windows.Forms.DockStyle.Right;
+			this.pictureBox3.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox3.Image")));
+			this.pictureBox3.Location = new System.Drawing.Point(1004, 0);
+			this.pictureBox3.Name = "pictureBox3";
+			this.pictureBox3.Size = new System.Drawing.Size(32, 29);
+			this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+			this.pictureBox3.TabIndex = 4;
+			this.pictureBox3.TabStop = false;
+			this.pictureBox3.Click += new System.EventHandler(this.pictureBox3_Click);
+			// 
+			// pictureBox4
+			// 
+			this.pictureBox4.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+			this.pictureBox4.Cursor = System.Windows.Forms.Cursors.Hand;
+			this.pictureBox4.Dock = System.Windows.Forms.DockStyle.Right;
+			this.pictureBox4.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox4.Image")));
+			this.pictureBox4.Location = new System.Drawing.Point(976, 0);
+			this.pictureBox4.Name = "pictureBox4";
+			this.pictureBox4.Size = new System.Drawing.Size(28, 29);
+			this.pictureBox4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+			this.pictureBox4.TabIndex = 5;
+			this.pictureBox4.TabStop = false;
+			this.pictureBox4.Click += new System.EventHandler(this.pictureBox4_Click);
+			// 
+			// panel4
+			// 
+			this.panel4.BackColor = System.Drawing.SystemColors.GrayText;
+			this.panel4.Controls.Add(this.label3);
+			this.panel4.Controls.Add(this.label2);
+			this.panel4.Controls.Add(this.label1);
+			this.panel4.Dock = System.Windows.Forms.DockStyle.Top;
+			this.panel4.Location = new System.Drawing.Point(47, 29);
+			this.panel4.Name = "panel4";
+			this.panel4.Size = new System.Drawing.Size(1010, 33);
+			this.panel4.TabIndex = 3;
+			// 
+			// label1
+			// 
+			this.label1.Cursor = System.Windows.Forms.Cursors.Hand;
+			this.label1.Dock = System.Windows.Forms.DockStyle.Left;
+			this.label1.Font = new System.Drawing.Font("Comic Sans MS", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+			this.label1.ForeColor = System.Drawing.SystemColors.Control;
+			this.label1.Location = new System.Drawing.Point(0, 0);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(80, 33);
+			this.label1.TabIndex = 0;
+			this.label1.Text = "Menu 1";
+			this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+			// 
+			// label2
+			// 
+			this.label2.Cursor = System.Windows.Forms.Cursors.Hand;
+			this.label2.Dock = System.Windows.Forms.DockStyle.Left;
+			this.label2.Font = new System.Drawing.Font("Comic Sans MS", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+			this.label2.ForeColor = System.Drawing.SystemColors.Control;
+			this.label2.Location = new System.Drawing.Point(80, 0);
+			this.label2.Name = "label2";
+			this.label2.Size = new System.Drawing.Size(80, 33);
+			this.label2.TabIndex = 1;
+			this.label2.Text = "Menu 2";
+			this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+			// 
+			// label3
+			// 
+			this.label3.Cursor = System.Windows.Forms.Cursors.Hand;
+			this.label3.Dock = System.Windows.Forms.DockStyle.Left;
+			this.label3.Font = new System.Drawing.Font("Comic Sans MS", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+			this.label3.ForeColor = System.Drawing.SystemColors.Control;
+			this.label3.Location = new System.Drawing.Point(160, 0);
+			this.label3.Name = "label3";
+			this.label3.Size = new System.Drawing.Size(80, 33);
+			this.label3.TabIndex = 2;
+			this.label3.Text = "Menu 3";
+			this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.ClientSize = new System.Drawing.Size(800, 450);
-			this.Text = "Form1";
+			this.BackColor = System.Drawing.SystemColors.WindowFrame;
+			this.ClientSize = new System.Drawing.Size(1115, 572);
+			this.Controls.Add(this.panel4);
+			this.Controls.Add(this.panel3);
+			this.Controls.Add(this.pictureBox1);
+			this.Controls.Add(this.panel2);
+			this.Controls.Add(this.panel1);
+			this.Cursor = System.Windows.Forms.Cursors.Default;
+			this.ForeColor = System.Drawing.SystemColors.Desktop;
+			this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+			this.Name = "Form1";
+			this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+			this.Load += new System.EventHandler(this.Form1_Load);
+			this.panel2.ResumeLayout(false);
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+			this.panel3.ResumeLayout(false);
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
+			this.panel4.ResumeLayout(false);
+			this.ResumeLayout(false);
+
 		}
 
 		#endregion
+
+		private System.Windows.Forms.Panel panel1;
+		private System.Windows.Forms.Panel panel2;
+		private System.Windows.Forms.PictureBox pictureBox1;
+		private System.Windows.Forms.Panel panel3;
+		private System.Windows.Forms.Button rs_btn1;
+		private System.Windows.Forms.Button button2;
+		private System.Windows.Forms.Button button1;
+		private System.Windows.Forms.PictureBox pictureBox2;
+		private System.Windows.Forms.PictureBox pictureBox4;
+		private System.Windows.Forms.PictureBox pictureBox3;
+		private System.Windows.Forms.Panel panel4;
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.Label label3;
+		private System.Windows.Forms.Label label2;
 	}
 }
 
--- Form1.cs
+++ Form1.cs
@@ -17,5 +17,58 @@ namespace TDP_Vista_Software_UI
 			InitializeComponent();
 		}
 
+		private void Form1_Load(object sender, EventArgs e)
+		{
+			this.WindowState = FormWindowState.Normal;
+		}
+
+		private void rs_btn1_Paint(object sender, PaintEventArgs e)
+		{
+			//Font font = new Font("Cosmic Sans", 9);
+			//Brush brush = new System.Drawing.SolidBrush(System.Drawing.Color.White);
+
+			//e.Graphics.TranslateTransform(100, 50);
+			//e.Graphics.RotateTransform(90);
+			//e.Graphics.DrawString("Btn 1", font, brush, 0, 0);
+		}
+
+		private void pictureBox2_Click(object sender, EventArgs e)
+		{
+			Application.Exit();
+		}
+
+		private void pictureBox3_Click(object sender, EventArgs e)
+		{
+			if (this.WindowState == FormWindowState.Normal)
+			{
+				this.WindowState = FormWindowState.Minimized;
+			}
+			else if(this.WindowState == FormWindowState.Minimized)
+			{
+				this.WindowState = FormWindowState.Normal;
+			}
+		}
+
+		private void pictureBox4_Click(object sender, EventArgs e)
+		{
+			if (this.WindowState == FormWindowState.Normal)
+			{
+				this.WindowState = FormWindowState.Maximized;
+			}
+			else if(this.WindowState == FormWindowState.Maximized)
+			{
+				this.WindowState = FormWindowState.Normal;
+			}
+			else if(this.WindowState == FormWindowState.Maximized)
+			{
+				this.WindowState = FormWindowState.Minimized;
+			}
+			else if(this.WindowState == FormWindowState.Minimized)
+			{
+				this.WindowState = FormWindowState.Maximized;
+			}
+		}
+
+
 	}
 }
